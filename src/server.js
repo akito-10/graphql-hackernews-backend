ad4a667520f5841229c035dbe3f216779b3262f6
github akito-10 <akt.fukuda@gmail.com>
2022-08-28 14:21:47 +0900

    ✨ add schema

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+let links = [
+  {
+    id: "link-0",
+    description: "GraphQLチュートリアルをUdemyで学ぶ",
+    url: "www.udemy-graphql-tutorial.com",
+  },
+];
+
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => "HOckerNewsクローン",
+    feed: () => links,
   },
 };
 
